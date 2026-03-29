cf0a0b1026443bb30ffc98fdab22350eda6c8ad6
faresjuha93-hash <faresjuha93@gmail.com>
2026-03-29 02:06:01 -0500

    Updated port for deployment

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res) => {
     res.status(404).render("404");
 })
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server Running on http://localhost:${PORT}`);
 })
