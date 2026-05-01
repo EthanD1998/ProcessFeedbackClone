ac24a13f549bb81c9effac06506384b8752816b9
faresjuha93-hash <faresjuha93@gmail.com>
2026-05-01 03:08:35 -0500

    Added dark mode + cleaned CSS

---
+++ public/js/darkmode.js
@@ -0,0 +1,19 @@
+﻿const darkModeToggle = document.querySelector("#darkModeToggle");
+
+if (localStorage.getItem("darkMode") === "enabled") {
+    document.body.classList.add("dark-mode");
+}
+
+if (darkModeToggle) {
+    darkModeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("darkMode", "enabled");
+            darkModeToggle.innerText = "☀️ Light";
+        } else {
+            localStorage.setItem("darkMode", "disabled");
+            darkModeToggle.innerText = "🌙 Dark";
+        }
+    });
+}
